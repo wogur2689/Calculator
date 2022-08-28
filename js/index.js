73cfeea808ed806cb5216b702fc17d8d5074e1f1
wogur2689 <81075170+wogur2689@users.noreply.github.com>
2022-08-28 15:14:58 +0900

    계산 이벤트 추가

--- js/index.js
+++ js/index.js
@@ -1,7 +1,15 @@
-/* 입력한 숫자 가져오기 */
-function fn_number() {
-    $(".box").click(function() {
-        var num = $(".box").children("span").text()
-        console.log(num)
-    })
+/* 입력한 숫자 띄우기 */
+function fn_number(num) {
+    $("#window").text($("#window").text()+num)
+}
+
+/* 입력한 숫자 계산하기*/
+function fn_cal() {
+    const result = eval($("#window").text())
+    $("#window").text(result)
+}
+
+/* 계산 리셋 */
+function fn_reset() {
+    $("#window").text("")
 }
